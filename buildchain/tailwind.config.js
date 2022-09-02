eaae2ec11567fe98d1fa98bfc5ecc0b1a56216b0
Stefanie Gevaert <>
2022-09-02 11:32:58 +0200

    chore: updated buildchain depedencies

--- buildchain/tailwind.config.js
+++ buildchain/tailwind.config.js
@@ -1,36 +1,44 @@
-// module exports
 module.exports = {
-    mode: 'jit',
-    purge: {
-        content: [
-            '../src/templates/**/*.{twig,html}',
-            './src/vue/**/*.{vue,html}',
-        ],
-        layers: [
-            'base',
-            'components',
-            'utilities',
-        ],
-        mode: 'layers',
-        options: {
-            whitelist: [
-                './src/css/components/*.css',
-            ],
-        }
-    },
-    important: true,
+    content: [
+        './src/**/*.{vue,js,ts}',
+    ],
+    safelist: [
+        'bg-gray-100',
+        'block',
+        'md:block',
+        'col-span-3',
+        'md:col-span-6',
+        'font-semibold',
+        'gap-x-4',
+        'grid',
+        'grid-cols-3',
+        'md:grid-cols-6',
+        'hidden',
+        'p-4',
+        'px-4',
+        'py-2',
+        'text-gray-600',
+        'text-xs',
+        'uppercase'
+    ],
     theme: {
+        // Extend the default Tailwind config here
         extend: {
             colors: {
-              red: {
-                  craft: '#e5422b',
-              }
+                red: {
+                    craft: '#e5422b',
+                }
             },
             minHeight: (theme) => ({
                 12: theme('height.12')
             })
-        }
+        },
     },
-    corePlugins: {},
-    plugins: [],
+    important: true,
+    plugins: [
+        require('@tailwindcss/typography'),
+        require('@tailwindcss/forms'),
+        require('@tailwindcss/line-clamp'),
+        require('@tailwindcss/aspect-ratio'),
+    ],
 };
